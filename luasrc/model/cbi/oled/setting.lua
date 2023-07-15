5432dbeca52bc38b1698fd8c666743ad461a7fdb
Kazagumo <13860126292@163.com>
2023-07-16 04:29:08 +0800

    Update setting.lua

--- luasrc/model/cbi/oled/setting.lua
+++ luasrc/model/cbi/oled/setting.lua
@@ -42,9 +42,7 @@ o = s:taboption("info", Flag, "cpufreq", translate("CPU frequency"))
 o.default=0
 o = s:taboption("info", Flag, "netspeed", translate("Network speed"), translate("1Mbps(m/s)=1,000Kbps(k/s)=1,000,000bps(b/s)"))
 o.default=0
-o = s:taboption("info", ListValue, "netsource", translate("which eth to monitor"))                       
-o:value("eth0","eth0")                                                                                   
-o:value("eth1","eth1")                                                                                   
+o = s:taboption("info", Value, "netsource", translate("which eth to monitor"))                                                                                                      
 o:depends("netspeed",'1')
 o.default='eth0'
 o = s:taboption("info", Value, "time", translate("Display interval(s)"), translate('Screensaver will activate in set seconds'))
